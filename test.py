b11f85411861ef6a1b156ce11cac7d848eef9758
Randy Rhythm <matt.steger@gmail.com>
2021-11-19 10:15:57 -0500

    updated tracking group for popular military and thebright

--- test.py
+++ test.py
@@ -7,14 +7,19 @@ BASE = "http://0.0.0.0:5000/"
 #response = requests.get(BASE + "feed/93425")
 #print(response.json())
 #input()
-response = requests.put(BASE + "feed/93425", {"feedid":105, "url":"https://thebright.com/feed/", "trackinggroup":93425})
+response = requests.put(BASE + "feed/93556", {"feedid":107, "url":"https://thebright.com/feed/", "trackinggroup":93556})
 print(response.json())
+#response = requests.put(BASE + "feed/93425", {"feedid":105, "url":"https://thebright.com/feed/", "trackinggroup":93425})
+#print(response.json())
 response = requests.put(BASE + "feed/98765", {"feedid":106, "url":"https://popularmilitary.com/feed/", "trackinggroup":98765})
 print(response.json())
 #testing
 #more testing
-response = requests.get(BASE + "feed/93425")
+
+response = requests.get(BASE + "feed/93556")
 print(response.json())
-response = requests.patch(BASE + "feed/98765")
+response = requests.get(BASE + "feed/98765")
+print(response.json())
+response = requests.patch(BASE + "feed/93425",{"url":"https://popularmilitary.com/feed/"})
 print(response.json())
 
