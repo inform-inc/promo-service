9a512c240976ba3eb94f2f4632eedc9fd2c60225
Randy Rhythm <matt.steger@gmail.com>
2021-11-19 14:16:30 -0500

    updated test.py

--- test.py
+++ test.py
@@ -9,10 +9,10 @@ BASE = "http://0.0.0.0:5000/"
 #input()
 response = requests.put(BASE + "feed/93556", {"feedid":107, "url":"https://thebright.com/feed/", "trackinggroup":93556})
 print(response.json())
-#response = requests.put(BASE + "feed/93425", {"feedid":105, "url":"https://thebright.com/feed/", "trackinggroup":93425})
-#print(response.json())
-response = requests.put(BASE + "feed/98765", {"feedid":106, "url":"https://popularmilitary.com/feed/", "trackinggroup":98765})
+response = requests.put(BASE + "feed/77008", {"feedid":108, "url":"https://thebright.com/feed/", "trackinggroup":77008})
 print(response.json())
+#response = requests.put(BASE + "feed/98765", {"feedid":106, "url":"https://popularmilitary.com/feed/", "trackinggroup":98765})
+#print(response.json())
 
 response = requests.get(BASE + "feed/93556")
 print(response.json())
