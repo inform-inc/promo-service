2eaecceb4f079c28ab945647dfdc13d708b101ec
Randy Rhythm <matt.steger@gmail.com>
2021-11-30 20:37:18 -0500

    updated test.py with pop mil

--- test.py
+++ test.py
@@ -11,13 +11,13 @@ response = requests.put(BASE + "feed/93556", {"feedid":107, "url":"https://thebr
 print(response.json())
 response = requests.put(BASE + "feed/77008", {"feedid":108, "url":"https://thebright.com/feed/", "trackinggroup":77008})
 print(response.json())
-#response = requests.put(BASE + "feed/98765", {"feedid":106, "url":"https://popularmilitary.com/feed/", "trackinggroup":98765})
+response = requests.put(BASE + "feed/93425", {"feedid":106, "url":"https://popularmilitary.com/feed/", "trackinggroup":93425})
 #print(response.json())
 
 response = requests.get(BASE + "feed/93556")
 print(response.json())
 response = requests.get(BASE + "feed/98765")
 print(response.json())
-response = requests.patch(BASE + "feed/93425",{"url":"https://popularmilitary.com/feed/"})
-print(response.json())
+#response = requests.patch(BASE + "feed/93425",{"url":"https://popularmilitary.com/feed/"})
+#print(response.json())
 
